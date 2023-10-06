9daeb2eda1ea27d33f8bf906b7da5ba915b51424
azharazharuddin22 <azharazharuddin22@gmail.com>
2023-10-06 19:27:46 +0530

    image upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+import { AngularFireStorage } from "@angular/fire/compat/storage"
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'imageupload';
+
+  constructor(private fireStorage:AngularFireStorage){}
+
+   async onFileChange(event:any){
+    const file = event.target.files[0]
+    if(file){
+      const path = `yt/${file.name}`
+      const uploadTask =await this.fireStorage.upload(path,file)
+      const url = await uploadTask.ref.getDownloadURL()
+      console.log(url)
+    }
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCJgkioYRB68fopHSQKAvdGLn381VSLHYM",
+    authDomain: "angularcrud-6be69.firebaseapp.com",
+    databaseURL: "https://angularcrud-6be69-default-rtdb.firebaseio.com",
+    projectId: "angularcrud-6be69",
+    storageBucket: "angularcrud-6be69.appspot.com",
+    messagingSenderId: "535243672600",
+    appId: "1:535243672600:web:c40188e462143f6225076d"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
